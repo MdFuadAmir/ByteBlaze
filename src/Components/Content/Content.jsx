c7dcafc16f8e25f6a86789100ebd5abc898b949c
Md Fuad Amir <mdfuadamir@gmail.com>
2024-11-16 23:04:54 +0600

    added contact and author section

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,9 +1,11 @@
+import Markdown from "react-markdown";
 import { useLoaderData } from "react-router-dom";
+import rehypeRaw from "rehype-raw";
 
 
 const Content = () => {
     const blog = useLoaderData();
-    const {cover_image,description,title,published_at,tags} = blog;
+    const {cover_image,title,tags,body_html} = blog;
     return (
         <div>
             <div className="mx-auto group hover:no-underline focus:no-underline border-2 border-gray-500 border-opacity-30  rounded-lg duration-300 p-2">
@@ -13,9 +15,11 @@ const Content = () => {
                     {
                         tags.map((tag,index) => <a key={index} rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline">#{tag}</a>)
                     }
-                    
                 </div>
 				<h3 className="text-2xl font-semibold  group-hover:underline group-focus:underline">{title}</h3>
+                <Markdown rehypePlugins={rehypeRaw}>
+                {body_html}
+                </Markdown>
 				</div>
 			</div>
         </div>
